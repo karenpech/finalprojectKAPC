0799ab997d0e8e6ca9d20041810e7cdf6df7c1de
karenpech <kand.pcnv@gmail.com>
2023-01-21 11:49:08 -0600

    Modificaciones al tercer intento - Comparativa Sin barra

--- main.py
+++ main.py
@@ -104,4 +104,5 @@ cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 img = img*mask2[:,:,np.newaxis]
 
-plt.imshow(img),plt.colorbar(),plt.show()
+plt.imshow(img),plt.show()
+'''plt.imshow(img),plt.colorbar(),plt.show'''
