21c7c63dcbcd861f52c7e1d399ae87c41d3be8f9
karenpech <kand.pcnv@gmail.com>
2023-01-21 10:57:57 -0600

    Segundo intento

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 '''
+#FIRST ATTEMPT
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
@@ -20,6 +21,7 @@ for i in range(6):
     plt.xticks([]),plt.yticks([])
 plt.show()
 '''
+#SECOND ATTEMPT
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
@@ -55,3 +57,5 @@ plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(thresh,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 plt.show()
+
+#THIRD ATTEMPT
