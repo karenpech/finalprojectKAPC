4a6f9a5b29308be4cdd65038c693874509baf470
karenpech <kand.pcnv@gmail.com>
2023-01-21 10:52:23 -0600

    Segundo intento

---
+++ main.py
@@ -0,0 +1,57 @@
+'''
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('RX.jpg',0)
+edges = cv.Canny(img,40,100)
+img = cv.imread('RX.jpg',0)
+#kernel = np.ones((5,5),np.float32)/25
+#dst = cv.filter2D(img,-1,kernel)
+ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC)
+ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO)
+ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV)
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+'''
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('RX.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+cv.destroyAllWindows()
+
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
+# sure background area
+sure_bg = cv.dilate(opening,kernel,iterations=3)
+# Finding sure foreground area
+dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv.subtract(sure_bg,sure_fg)
+
+# Marker labelling
+ret, markers = cv.connectedComponents(sure_fg)
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+markers = cv.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(thresh,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
